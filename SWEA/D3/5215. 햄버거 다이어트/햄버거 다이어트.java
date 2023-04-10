3acc60014285db5d9dfe61168cb3b9e97375cd28
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-04-10 13:45:42 +0900

    [D3] Title: 햄버거 다이어트, Time: 213 ms, Memory: 22,760 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -17,19 +17,22 @@ public class Solution {
 				cal[i] = sc.nextInt();
 			}
 
-			int[][] dp = new int[n + 1][l + 1];
+			int[][] dp = new int[2][l + 1];
 
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= l; j++) {
 					if (j >= cal[i]) {
-						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cal[i]] + taste[i]);
+						dp[1][j] = Math.max(dp[0][j], dp[0][j - cal[i]] + taste[i]);
 					} else {
-						dp[i][j] = dp[i - 1][j];
+						dp[1][j] = dp[0][j];
 					}
+				}
+				for (int j = 1; j <= l; j++) {
+					dp[0][j] = dp[1][j];
 				}
 			}
 
-			System.out.printf("#%d %d\n", tc, dp[n][l]);
+			System.out.printf("#%d %d\n", tc, dp[1][l]);
 		}
 	}
 }
